066554f5d355c87b8b812d22acafff47c1fc5b23
yskim <yeonsker@gmail.com>
2023-02-24 17:59:29 +0900

    company commit

--- src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -25,7 +25,7 @@ class MemberServiceIntegrationTest {
     void 회원가입() {
         //given
         Member member = new Member();
-        member.setName("KYS");
+        member.setName("spring1");
 
         //when
         Long saveId = memberService.join(member);
